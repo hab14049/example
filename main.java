f4be445ab67af3ccd0f2c74ea41279ab81b56160
Daniel Haberfellner <hab14049@spengergasse.at>
2013-05-16 17:24:55 +0200

    version 2

--- main.java
+++ main.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 
-public class main {
+
+public class main2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Wolrd");
-
+		Scanner user_input = new Scanner(System.in);
+		String name;
+		System.out.println("Enter your name");
+		name = user_input.next();
+		System.out.println("Hello "+name+".");
 	}
 
 }
